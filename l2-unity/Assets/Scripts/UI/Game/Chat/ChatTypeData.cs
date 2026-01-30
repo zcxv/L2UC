4eaba429bdb783341b3e8422b15a75ea0b7a115e
gawric <gawric@mail.ru>
2026-01-31 01:52:33 +0500

    Added: Full functionality of the All/Trade/Party chat. Thanks to user AbsolutePower

--- l2-unity/Assets/Scripts/UI/Game/Chat/ChatTypeData.cs
+++ l2-unity/Assets/Scripts/UI/Game/Chat/ChatTypeData.cs
@@ -6,7 +6,7 @@ public class ChatTypeData
     public int Type;
     public string Text;
     public string Color;
-
+		//Test 1
     public ChatTypeData(int id, int type, string text, string color)
     {
         Id = id;
